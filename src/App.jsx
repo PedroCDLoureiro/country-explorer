83f77db66c88eda706b5eb3a70d9224e116fd55c
Pedro Loureiro <pedroloureiro_1912@hotmail.com>
2026-08-20 22:07:51 -0300

    search

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import CountryCard from './components/CountryCard'
 function App() {
 
     const [countries, setCountries] = useState([])
+    const [search, setSearch] = useState('')
+    const [region, setRegion] = useState('')
     
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
@@ -13,7 +15,7 @@ function App() {
         async function getCountries() {
             try{
                 const response = await fetch(
-                    'https://api.restcountries.com/countries/v5',
+                    'https://api.restcountries.com/countries/v5?limit=100',
                     {
                         headers: {
                             Authorization: `Bearer ${import.meta.env.VITE_REST_COUNTRIES_API_KEY}`
@@ -39,16 +41,52 @@ function App() {
         getCountries()
     }, [])
 
+    const filteredCountries = countries.filter((country) => {
+
+        // Filtro de pesquisa
+
+        const searchFilter = country.names.common.toLowerCase().includes(search.toLowerCase())
+
+        // Filtro de região
+        const regionFilter = region === '' || country.region === region
+
+        return searchFilter && regionFilter
+    })
+
     return (
         <main>
             <h1>Country Explorer</h1>
 
+            <input 
+                type="text" 
+                placeholder="Pesquisar país" 
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+
+            <select
+                value={region}
+                onChange={(e) => setRegion(e.target.value)}
+            >
+                <option value="">Todas as regiões</option>
+                <option value="Africa">África</option>
+                <option value="Americas">Américas</option>
+                <option value="Asia">Ásia</option>
+                <option value="Europe">Europa</option>
+                <option value="Oceania">Oceania</option>
+            </select>
+
             {loading && <p>Carregando países...</p>}
             {error && <p>Não foi possível carregar os países.</p>}
 
-            {countries.map((country) => (
+            {filteredCountries.map((country) => (
                 <CountryCard key={country.uuid} country={country} />
             ))}
+
+            {filteredCountries.length === 0 && !loading && !error && (
+                <p>Nenhum país encontrado.</p>
+            )}
+
         </main>
     )
 }
